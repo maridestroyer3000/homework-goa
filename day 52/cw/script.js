a861c5506933f97066bf4d65a2c45c0679a8f634
piririrate <marrsikoo444@gmail.com>
2026-03-19 22:16:02 +0400

    commit

---
+++ day 52/cw/script.js
@@ -0,0 +1,11 @@
+// 1 let - მეორედ არ გამოცხადდება და შეცვლა შეუძლია
+// const - არც შეცვლა შეუძლია და არც მეორედ გამოცხადება
+
+// 2
+// let name = "saba";
+// let surname = "datukishvili";
+// let hair color = "brown";
+// let country = "georgia";
+// let hobby = "gaming";
+// 
+
---
+++ day 53/cw/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("butt")
+
+function changeColor(){
+    button.style.backgroundColor = "blue"
+}
+
+console.dir(button)
